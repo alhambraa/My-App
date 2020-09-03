bfb78678302bb6996637667d45963baa964dd486
alhambraa <hambra02@gmail.com>
2020-09-03 22:06:30 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,42 @@ import Card from './components/card/Card';
 import NextMeetUp from './components/NextMeetUp';
 import Members from './components/Members';
 import PastMeetUp from './components/PastMeetUp';
-import {Row, Container} from 'react-bootstrap'
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
+  const PastMeetUpData = [
+    {
+      id: 0,
+      header: '27 November 2017',
+      title: '#39 JakartaJS April Meetup with Kumparan',
+      content: '139',
+    },
+    {
+      id: 1,
+      header: '27 November 2017',
+      title: '#38 JakartaJS April Meetup with Blibli',
+      content: 113,
+    },
+    {
+      id: 2,
+      header: '27 November 2017',
+      title: '#37 JakartaJS April Meetup with Hacktiv8',
+      content: 110,
+    }
+  ]
   return (
     <div className="App">
        <NavbarTop/>
        <br></br>
         <div className="container">
-            <Card/>
+            <Card
+              event="Hactiv8 Meeting"
+              location="Jakarta, Indonesia"
+              member="Members : 1,078"
+              organizer="Organizers : Alhambra"
+              button="Join Us"
+            />
             <h4 className="text-left">Next Meetup</h4>
             <NextMeetUp/>
             <h4 className="text-left">About Meet Up</h4>
@@ -28,25 +52,7 @@ function App() {
             <Members/>
             <h4 className="text-left">Past Meetups</h4>
             <h6 className="text-right">See all</h6>
-            <Container className="text-left">
-              <Row className="ml-0">
-                <PastMeetUp
-                  header="27 November 2020"
-                  title="#39 JakartaJS April Meetup with Kumparan"
-                  content="139 went"
-                />
-                <PastMeetUp
-                  header="27 Oktober 2020"
-                  title="#38 JakartaJS April Meetup with BliBli"
-                  content="113 went"
-                />
-                <PastMeetUp
-                  header="27 November 2020"
-                  title="#39 JakartaJS April Meetup with Hacktiv8"
-                  content="110 went"
-                />
-              </Row>
-            </Container>
+            <PastMeetUp data={PastMeetUpData}/>
             <hr style={{background:"black"}}></hr>
           <br />
           <center>

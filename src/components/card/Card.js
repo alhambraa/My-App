ebcaf1359bb3fe747fcd3322caaf4e7d29afd87b
alhambraa <hambra02@gmail.com>
2020-09-14 12:11:15 +0700

    Update Style Component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import Avatar from '../avatar/Avatar'
 import Images from '../img/index.jpg'
-// import Button from '../button/Button'
-import Button from 'react-bootstrap/Button'
 import PropTypes from 'prop-types'
-// import "./card.css";
+import {CardBackground,CardContent} from "../style/CardStyle";
+import Button from "../style/Button"
 
-const Card = ({event,location,member,organizer,button}) => {
+
+const Card = ({event,location,member,organizer}) => {
     return (
-        <div className="card flex-row flex-wrap text-left bg-secondary mb-4">
+        <CardBackground>
             <div ><Avatar src={Images} alt={Images}/></div>
-            <div className="card-block px-2">
+            <CardContent>
                 <h5 className="card-title mt-2"><b>{event}</b></h5>
                 <p className="card-text">{location}</p>
                 <p className="card-text">{member}</p>
                 <p className="card-text">{organizer}</p>
-                <Button variant="primary" size="sm">{button}</Button>{' '}
-            </div>
-        </div>
+                <Button primary="primary" text="Join Us!"></Button>
+                <Button text="General"></Button>
+            </CardContent>
+        </CardBackground>
     )
 };
 
@@ -26,7 +27,6 @@ Card.propTypes  = {
     event : PropTypes.string.isRequired,
     member : PropTypes.string.isRequired,
     organizer : PropTypes.string.isRequired,
-    button : PropTypes.string.isRequired,
 }
 
 Card.defaultProps = {
@@ -34,18 +34,6 @@ Card.defaultProps = {
     event : "Event Here",
     member : "Member Here",
     organizer : "Organizer Here",
-    button : "Name Button"
 }
 
 export default Card
-
-/* <div className="card flex-row flex-wrap text-left bg-secondary mb-4">
-<Avatar/>
-<div className="card-block px-2">
-    <h5 className="card-title mt-2"><b>Hactiv8 Meeting</b></h5>
-    <p className="card-text">Location : Jakarta, Indonesia</p>
-    <p className="card-text">Members : 1,078</p>
-    <p className="card-text">Organizers : Alhambra</p>
-    <Button variant="primary" size="sm">Join Us</Button>{' '}
-</div>
-</div> */

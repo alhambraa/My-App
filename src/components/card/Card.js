6ac6a64383e3d11b639c615c90c05b7909a89efe
alhambraa <hambra02@gmail.com>
2020-10-02 14:13:30 +0700

    Unit Testing

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -11,10 +11,10 @@ const Card = ({event,location,member,organizer}) => {
         <CardBackground>
             <div ><Avatar src={Images} alt={Images}/></div>
             <CardContent>
-                <h5 className="card-title mt-2"><b>{event}</b></h5>
-                <p className="card-text">{location}</p>
-                <p className="card-text">{member}</p>
-                <p className="card-text">{organizer}</p>
+                <h5><b>{event}</b></h5>
+                <p className="card-text" id="location">Location : {location}</p>
+                <p className="card-text" id="member">Member : {member}</p>
+                <p className="card-text" id="organizer">Organizer : {organizer}</p>
                 <Button primary="primary" text="Join Us!"></Button>
                 <Button text="General"></Button>
             </CardContent>
@@ -27,13 +27,6 @@ Card.propTypes  = {
     event : PropTypes.string.isRequired,
     member : PropTypes.string.isRequired,
     organizer : PropTypes.string.isRequired,
-}
-
-Card.defaultProps = {
-    avatar : "Image Here",
-    event : "Event Here",
-    member : "Member Here",
-    organizer : "Organizer Here",
 }
 
 export default Card

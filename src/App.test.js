2653b7289d5cc9be08f10dafa1914178f88f656f
alhambraa <hambra02@gmail.com>
2020-10-02 14:11:53 +0700

    Unit Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow, mount } from 'enzyme';
+import toJson from "enzyme-to-json";
 import App from './App';
-import HelloWorld from './HelloWorld';
+import Card from './components/card/Card'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<HelloWorld />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+it('Pages App running without crash', () => {
+  shallow(<App />);
 });
+
+it('Render Join Us Title', () => {
+  const wrapper = mount(<App />);
+  const header = <h5><b>Hacktiv8 Meeting</b></h5>
+  expect(wrapper.contains(header)).toEqual(true);
+});
+
+describe('Content Description Test', () => {
+  it('Location', () => {
+    const wrapper = shallow(<Card />);
+    const location = wrapper.find("#location").first().text();
+    expect(location).toEqual("Location : ");
+  });
+
+  it('Member', () => {
+    const wrapper = shallow(<Card />);
+    const member = wrapper.find("#member").text();
+    expect(member).toEqual("Member : ");
+  });
+
+  it('Organizer', () => {
+    const wrapper = shallow(<Card />);
+    const organizer = wrapper.find("#organizer").text();
+    expect(organizer).toEqual("Organizer : ");
+  });
+})
+
+
+
+
+
+
+
